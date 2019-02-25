efdb192a4bcbc883a572a5245ac2a7045afcc736
Dowan Kim <dowankim@Dowanui-MacBook-Pro.local>
2019-02-24 20:10:09 -0500

    final work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,45 @@
 // Global Variables
 
-var animals = ["dog", "cat", "rabbit", "tiger", "lion", "wolf", "dolphin", "shark", "fish", "monkey"]
+var animals = ["dog", "cat", "drangon", "rabbit", "tiger", "lion", "wolf", "dolphin", "shark", "fish", "monkey", "dragon", "armadillo", "bird"]
 
 function displayAnimalInfo() {
     var animal = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=i9wNHxnSqAJ5ZjXgDKQOszq9pQjDXhlG&q=" + animal + "&limit=10&offset=&lang=en";
 
-    $.ajax({
+    $.ajax({ //API setting
         url: queryURL,
         method: "GET"
     }).then(function(response) {
         
-        $("#animal-view").empty();
+        $("#animal-view").empty(); //When user clicked new button, the previous images are cleared
 
-        var animalDiv = $("<div>");
-        animalDiv.addClass("animal");
-
+        
+        //making 10 div/images/p for individual images
         for (var j = 0; j < 10; j++) {
-        var rating = response.data[j].rating;
+        var animalDiv = $("<div>");
+        animalDiv.addClass("animal col-lg-4 col-md-6 col-sm-12");
+
+        var rating = response.data[j].rating; //getting ratings of images
         console.log(rating);
 
         var pOne = $("<p>")
             pOne.text("Rating: " + rating);
 
-        animalDiv.append(pOne);
+        animalDiv.append(pOne); //child of new div
+
+        animalDiv.appendTo($("#animal-view")); //to make animalDiv as a child of #animal-view
        
-        var imgURL = response.data[j].images.fixed_width_still.url;
-        var clickedURL = response.data[j].images.fixed_width.url;
+        var imgURL = response.data[j].images.fixed_width_still.url; //getting fixed images (still image)
+        var clickedURL = response.data[j].images.fixed_width.url; //getting action gifs
         var image = $("<img>")
             image.addClass("gifImage");
             image.attr("src", imgURL);
-            image.attr("data-still", imgURL);
+            image.css('max-width','100%');  // to make images in the div
+            image.attr("data-still", imgURL);  // by changing state of the image from still to animate or anmiate to still using function below
             image.attr("data-animate", clickedURL);
             image.attr("data-state", "still");
 
-        animalDiv.append(image);
+        animalDiv.append(image); //child of animal div
         }
         $("#animal-view").prepend(animalDiv);
     });
@@ -46,11 +51,11 @@ function renderButtons() {
     
     $("#buttons-view").empty();
 
-    for (var i =0; i < animals.length; i++) {
+    for (var i =0; i < animals.length; i++) { // making buttons
         
         var a = $("<button>");
 
-        a.addClass("animal-btn");
+        a.addClass("animal-btn btn btn-success");
 
         a.attr("data-name", animals[i]);
 
@@ -60,7 +65,7 @@ function renderButtons() {
     }
 }
 
-$("#add-animal").on("click", function(event){
+$("#add-animal").on("click", function(event){ //get user input and put the user input to new button
     event.preventDefault();
 
     var animal = $("#animal-input").val().trim();
@@ -71,7 +76,7 @@ $("#add-animal").on("click", function(event){
 });
 
 $(document).on("click", ".animal-btn", displayAnimalInfo);
-$(document).on("click", ".gifImage", function(){
+$(document).on("click", ".gifImage", function(){ //function for animation or still image
     var state = $(this).attr("data-state");
     if( state == "still") {
         $(this).attr('src', $(this).data('animate'));
